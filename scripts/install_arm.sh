0f928752174edd742540eb40f07ef48c5c79bf7e
Iurii Telenkov <telenkov88@gmail.com>
2024-03-19 01:33:37 +0800

    fix sudo prompt

--- scripts/install_arm.sh
+++ scripts/install_arm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
+sudo apt-get -y install gcc-arm-none-eabi libnewlib-arm-none-eabi
--- scripts/install_espidf.sh
+++ scripts/install_espidf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instructions from: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html
-sudo apt-get install git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+sudo apt-get -y install git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 
 pip3 install pyelftools
 git clone https://github.com/espressif/esp-idf.git
