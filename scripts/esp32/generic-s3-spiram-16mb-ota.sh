ae17925b6a9f450090a41231ea087f766d6780e7
Iurii Telenkov <telenkov88@gmail.com>
2024-03-18 22:26:30 +0800

    add version to ESP32S3

--- scripts/esp32/generic-s3-spiram-16mb-ota.sh
+++ scripts/esp32/generic-s3-spiram-16mb-ota.sh
@@ -7,5 +7,9 @@
 #                2023 Zach Moshe
 
 source ./scripts/esp32/esp32.sh
+# Read the new version name from version.txt
+VERSION_NAME=$(cat version.txt)
+# Use sed to replace the line containing MICROPY_HW_BOARD_NAME
+sed -i "/MICROPY_HW_BOARD_NAME/c\        MICROPY_HW_BOARD_NAME=\"$VERSION_NAME\"" micropython/ports/esp32/boards/ESP32_GENERIC_S3_16MiB_OTA/mpconfigboard.cmake
 
 build_esp32 "ESP32_GENERIC_S3_16MiB_OTA"
