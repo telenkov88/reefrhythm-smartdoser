502bb3b70da35111e601aabc95f3ba0b026da817
Iurii Telenkov <telenkov88@gmail.com>
2024-06-10 15:25:09 +0800

    Viper + direct pointer even faster

--- debug/debug_viper.py
+++ debug/debug_viper.py
@@ -10,6 +10,7 @@ except ImportError:
 from lib.decorator import timed_function
 
 TEST_CYCLES = 10000
+BUFFER_SIZE = 100
 
 
 @timed_function
@@ -38,13 +39,43 @@ def sum_int16_array_native(arr: array, length: int, cycles: int) -> int:
 def sum_int16_array_viper(arr: ptr16, length: int, cycles: int) -> int:
     total = int(0)
     ptr_arr = ptr16(arr)  # Explicit pointer type to ensure correct handling
-    for _count in range(cycles):
+    c = int(0)
+    while c < cycles:
         total = int(0)
-        for i in range(length):
-            total += int(ptr_arr[i])  # Explicit conversion to ensure correct type handling
+        k = int(0)
+        while k < length:
+            total += int(ptr_arr[k])  # Explicit conversion to ensure correct type handling
+            k += 1
+        c += 1
     return total
 
 
+@timed_function
+@micropython.viper
+def sum_int16_array_viper_direct_pointer(arr: ptr16, l: uint, cycles: uint) -> int:
+    initial_ptr = uint(arr)  # Store the initial pointer
+    total = int(0)
+    c = uint(0)
+    r: int = 0  # Reset r for each cycle
+    while c < cycles:
+        r:int = 0  # Reset r for each cycle
+        p = ptr16(initial_ptr)  # Reset p to the start of the array for each cycle
+        pstop: uint = uint(p) + 2 * l  # Calculate the end pointer
+        while uint(p) < pstop:
+            r += p[0]  # Dereference the current pointer location
+            p = ptr16(uint(p) + 2)  # Increment pointer by the size of int16 (2 bytes)
+        c += 1
+    return r
+
+
+@timed_function
+def sum_int16_array_numpy(arr, cycles: int) -> int:
+    total = int(0)
+    for _count in range(cycles):
+        total = int(np.sum(arr))
+    return total
+
+# =========================== Floats ======================================
 @timed_function
 def avg_float_array(arr: array, length: int, cycles: int) -> float:
     _sum: float = 0.0
@@ -55,8 +86,8 @@ def avg_float_array(arr: array, length: int, cycles: int) -> float:
     return _sum / length if length > 0 else 0.0
 
 
-@micropython.native
 @timed_function
+@micropython.native
 def avg_float_array_native(arr: array, length: int, cycles: int) -> float:
     _sum: float = 0.0
     for _count in range(cycles):
@@ -70,12 +101,14 @@ print('\r\n', "="*50)
 print("Test Int16 array summ performance")
 print("="*50)
 
-BUFFER_SIZE = 100
+
 buffer = array.array('H', [0]*BUFFER_SIZE)
 
 
 for _ in range(BUFFER_SIZE):
-    buffer[_] = 65535
+    buffer[_] = random.randint(0, 65535)
+
+np_int_array = np.array(buffer)
 print(buffer)
 print("\r\nPython emitter")
 s1 = sum_int16_array(buffer, BUFFER_SIZE, TEST_CYCLES)
@@ -84,8 +117,16 @@ s2 = sum_int16_array_native(buffer, BUFFER_SIZE, TEST_CYCLES)
 print("\r\nViper emitter")
 s3 = sum_int16_array_viper(buffer, BUFFER_SIZE, TEST_CYCLES)
 
-if not (s1 == s2 == s3):
-    raise ValueError(f"Array summ not equal: {s1}, {s2}, {s3}")
+print("\r\nViper emitter with direct pointer")
+s4 = sum_int16_array_viper_direct_pointer(buffer, BUFFER_SIZE, TEST_CYCLES)
+print("\r\nNumpy emitter")
+s5 = sum_int16_array_numpy(np_int_array, TEST_CYCLES)
+
+tolerance = 2  # Allow a difference of 1 unit in the sum
+
+if not (abs(s1 - s2) == 0 and abs(s2 - s3) == 0 and abs(s3 - s4) == 0 and abs(s1 - s5) <= tolerance):
+    values = f"{s1}, {s2}, {s3}, {s4}, {s5}"
+    raise ValueError(f"Array summation not equal: {values}")
 else:
     print("Array sum: ", s1)
 
@@ -112,7 +153,7 @@ a2 = avg_float_array_native(buffer, BUFFER_SIZE, TEST_CYCLES)
 # Viper doesn't support float, but we can use store float values as int and then calculate floats in native
 @timed_function
 def avg_viper_for_float(arr: array, length: int, cycles: int) -> float:
-    _sum = sum_int16_array_viper(arr, BUFFER_SIZE, cycles)
+    _sum = sum_int16_array_viper_direct_pointer(arr, BUFFER_SIZE, cycles)
     _avg: float = 0.0
     for _count in range(cycles):
         _avg: float = 0.0
@@ -127,14 +168,6 @@ def avg_numpy_for_float(arr, cycles: int) -> float:
         _avg = np.mean(arr)
     return _avg
 
-@timed_function
-@micropython.native
-def avg_numpy_for_float_optimized(arr, cycles: int) -> float:
-    _avg: float = 0.0
-    for _count in range(cycles):
-        _avg = np.mean(arr)/10000
-    return float(_avg)
-
 
 print("\r\nViper optimized float emitter:")
 a3 = avg_viper_for_float(buffer_float_to_int, BUFFER_SIZE, TEST_CYCLES)
@@ -142,13 +175,10 @@ a3 = avg_viper_for_float(buffer_float_to_int, BUFFER_SIZE, TEST_CYCLES)
 print("\r\nNumpy float array calculation:")
 a4 = avg_numpy_for_float(np_array, TEST_CYCLES)
 
-print("\r\nNumpy float optimized array calculation:")
-a5 = avg_numpy_for_float_optimized(np_array_optimized, TEST_CYCLES)
-
 
 tolerance = 0.001  # Float error tolerance level
-if not (abs(a1 - a2) < tolerance and abs(a2 - a3) < tolerance and abs(a3 - a4) < tolerance and abs(a4 - a5) < tolerance):
-    values = f"{round(a1, 3)}, {round(a2, 3)}, {round(a3, 3)}, {round(a4, 3)}, {round(a5, 3)}"
+if not (abs(a1 - a2) < tolerance and abs(a2 - a3) < tolerance and abs(a3 - a4) < tolerance):
+    values = f"{round(a1, 3)}, {round(a2, 3)}, {round(a3, 3)}, {round(a4, 3)}"
     raise ValueError(f"Array average value not equal: ", values)
 else:
     print("Array avg: ", a4)
